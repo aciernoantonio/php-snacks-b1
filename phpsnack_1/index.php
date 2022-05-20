8075b7613b69254e4c9a78262f8e104533c5351b
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-20 12:39:45 +0200

    add array squad

---
+++ phpsnack_1/index.php
@@ -0,0 +1,36 @@
+<?php
+
+//creiamo un array di partite di basket
+$match = [
+
+    [
+        'homeSquad' => 'Olimpia Milano',
+        'guestSquad' => 'Cantù',
+        'homePoints' => '55',
+        'guestPoints' => '60'
+    ],
+
+    [
+        'homeSquad' => 'Roma',
+        'guestSquad' => 'Caserta Basket',
+        'homePoints' => '75',
+        'guestPoints' => '50'
+    ],
+
+    [
+        'homeSquad' => 'Torino',
+        'guestSquad' => 'Avellino',
+        'homePoints' => '45',
+        'guestPoints' => '60'
+    ],
+
+    [
+        'homeSquad' => 'Trieste',
+        'guestSquad' => 'Magenta',
+        'homePoints' => '30',
+        'guestPoints' => '60'
+    ],
+
+
+
+];
