822f4b4f4b73efb37ec8829fb5d045b602df0ed8
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-20 13:07:46 +0200

    add phpsnack_1

--- phpsnack_1/index.php
+++ phpsnack_1/index.php
@@ -34,3 +34,34 @@ $match = [
 
 
 ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    for ($i=0; $i < count($match); $i++) { ?> 
+    <div>
+        <span><?php echo $match[$i]["homeSquad"]; ?></span>
+        -
+        <span><?php echo $match[$i]["guestSquad"]; ?></span>
+        | 
+        <span><?php echo $match[$i]["homePoints"]; ?></span>
+        -
+        <span><?php echo $match[$i]["guestPoints"]; ?></span>
+    </div>
+    <br>
+    
+<?php } ?>
+    
+</body>
+</html>
